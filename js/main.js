f6cf35d8d6ce270dc81746d1216a90add0b8241d
LittleBitsOfGlory <riteshlogin@gmail.com>
2016-01-24 22:49:31 -0500

    Append data (json)
    Appending data to an element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$.ajax({
+  url: "https://cors-anywhere.herokuapp.com/http://closings.victorlourng.com/api/?all",
+  cache: false,
+  dataType: "json"
+})
+  .done(function( data ) {
+    $( "#data" ).append( data );
+  });
